be30a1151f6f3b957f5dccefd2dea6881ac27cef
Mayowa Temitope AKINYELE <robaireth@gmail.com>
2025-09-10 05:48:40 +0100

    fix: remove legacy sqlalchemy api imports for deployment

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,1 +1,13 @@
-from . import auth, users, teachers, students, parents, classes, events, messages
+from . import auth, users_prisma, teachers_prisma, students_prisma, parents_prisma, classes_prisma, events_prisma, messages_prisma, websockets, results_prisma
+
+# Re-export for easier importing in main
+auth = auth
+users = users_prisma
+teachers = teachers_prisma
+students = students_prisma
+parents = parents_prisma
+classes = classes_prisma
+events = events_prisma
+messages = messages_prisma
+results = results_prisma
+websockets = websockets
